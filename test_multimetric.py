d5314f23993998084e82912ff83b837b402e8c9f
Joan Massich <sik@visor.udg.edu>
2017-04-26 11:25:38 +0200

    Add the multimetric test to see if I added the PR properlly

---
+++ test_multimetric.py
@@ -0,0 +1,75 @@
+# Author: Raghav RV <rvraghav93@gmail.com>
+# License: BSD
+
+import numpy as np
+from matplotlib import pyplot as plt
+
+from sklearn.datasets import make_hastie_10_2
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import precision_score
+from sklearn.metrics.scorer import make_scorer
+from sklearn.tree import DecisionTreeClassifier
+
+
+print(__doc__)
+
+
+X, y = make_hastie_10_2(n_samples=8000, random_state=42)
+
+# The scorers can be a std scorer referenced by its name or one wrapped
+# by sklearn.metrics.scorer.make_scorer
+scoring = {'AUC Score': 'roc_auc', 'Precision': make_scorer(precision_score),
+           'recall': 'recall', 'F1 Score': 'f1'}
+
+# Multiple metric GridSearchCV, best_* attributes are exposed for the scorer
+# with key 'AUC Score' ('roc_auc')
+gs = GridSearchCV(DecisionTreeClassifier(random_state=42),
+                  param_grid={'min_samples_split': range(2, 403, 10)},
+                  scoring=scoring, cv=5, refit='AUC Score')
+gs.fit(X, y)
+
+results = gs.cv_results_
+
+plt.figure().set_size_inches(13, 13)
+plt.title("GridSearchCV evaluating using multiple scorers simultaneously",
+          fontsize=16)
+
+plt.xlabel("min_samples_split")
+plt.ylabel("Score")
+plt.grid()
+
+ax = plt.axes()
+ax.set_xlim(0, 402)
+ax.set_ylim(0.68, 1)
+
+# Get the regular numpy array from the MaskedArray
+X_axis = np.array(results['param_min_samples_split'].data, dtype=float)
+
+for scorer, color in (('AUC Score', 'g'), ('F1 Score', 'k'),
+                      ('Precision', 'r'), ('recall', 'blue')):
+    for sample, style in (('train', '--'), ('test', '-')):
+        sample_score_mean = results['mean_%s_%s' % (sample, scorer)]
+        sample_score_std = results['std_%s_%s' % (sample, scorer)]
+        ax.fill_between(X_axis, sample_score_mean - sample_score_std,
+                        sample_score_mean + sample_score_std,
+                        alpha=0.1 if sample == 'test' else 0, color=color)
+        ax.plot(X_axis, sample_score_mean, style, color=color,
+                alpha=1 if sample == 'test' else 0.7,
+                label="%s (%s)" % (scorer, sample))
+
+    best_index = np.nonzero(results['rank_test_%s' % scorer] == 1)[0][0]
+    best_score = results['mean_test_%s' % scorer][best_index]
+
+    # Plot a dotted vertical line at the best score for that scorer marked by x
+    ax.plot([X_axis[best_index], ] * 2, [0, best_score],
+            linestyle='-.', color=color, marker='x', markeredgewidth=3, ms=8)
+
+    # Annotate the score and the min_samples_split chosen by that scorer
+    ax.annotate("%d" % X_axis[best_index],
+                (X_axis[best_index] + 3, 0.685))
+    ax.annotate("%0.2f" % best_score,
+                (X_axis[best_index], best_score + 0.005))
+
+plt.legend(loc="best")
+plt.grid('off')
+plt.show()
