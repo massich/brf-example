1297bf59bef5980d78a656160fddb61b9cfa90a7
Joan Massich <sik@visor.udg.edu>
2017-04-26 14:45:51 +0200

    Try to find the break down of the imbalancing using moon dataset

--- moon_test.py
+++ moon_test.py
@@ -5,11 +5,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 from imblearn.datasets import make_imbalance
 from sklearn.datasets import make_moons
-from imblearn.metrics import classification_report_imbalanced
 from sklearn.ensemble import RandomForestClassifier, ExtraTreesClassifier
-from sklearn.model_selection import GridSearchCV
+from sklearn.model_selection import GridSearchCV, cross_val_score
 
 RANDOM_STATE = 42
+N_JOBS = 40
 
 classifiers = [
     ("Random Forest",
@@ -32,42 +32,45 @@ original_dataset = make_moons(noise=0.3, random_state=0, n_samples=int(1e6))
 
 
 def make_imbalance_dataset(d, ratio=.01):
-    return make_imbalance(d[0], d[1], ratio=ratio, random_state=42)
+    return make_imbalance(d[0], d[1], ratio=ratio, random_state=RANDOM_STATE)
 
 
-datasets = [ make_imbalance_dataset(original_dataset, ratio=0.01) ]
+def get_imbalance_ratios(num=6, min_ratio=0.01, max_ratio=0.5):
+    return np.logspace( np.log10(min_ratio), np.log10(max_ratio), num=num)
 
-# use a full grid over all parameters
-param_grid = {"max_depth": [5, None],
-              "criterion": ["gini", "entropy"],
-              "n_estimators": np.linspace(10,150, num=3, dtype=int)}
+datasets = [(r, make_imbalance_dataset(original_dataset, ratio=r))
+            for r in get_imbalance_ratios(num=10)]
 
-# Utility function to report best scores
-def report(results, n_top=3):
-    for i in range(1, n_top + 1):
-        candidates = np.flatnonzero(results['rank_test_score'] == i)
-        for candidate in candidates:
-            print("Model with rank: {0}".format(i))
-            print("Mean validation score: {0:.3f} (std: {1:.3f})".format(
-                  results['mean_test_score'][candidate],
-                  results['std_test_score'][candidate]))
-            print("Parameters: {0}".format(results['params'][candidate]))
-            print("")
+# we need stratifiedKFold to preserve class distribution within each fold
+from sklearn.model_selection import StratifiedKFold
+skf = StratifiedKFold(n_splits=5)
 
-from sklearn.externals import joblib
-for dataset in datasets:
+
+# make scorer to evaluate the results
+#  - http://scikit-learn.org/stable/modules/model_evaluation.html
+#  - https://github.com/scikit-learn-contrib/imbalanced-learn/blob/master/imblearn/metrics/classification.py
+#
+# Use custom scoring to track multiple metrics
+from sklearn.metrics import precision_score
+from sklearn.metrics.scorer import make_scorer
+
+# The scorers can be a std scorer referenced by its name or one wrapped
+# by sklearn.metrics.scorer.make_scorer
+scoring = {'AUC Score': 'roc_auc', 'Precision': make_scorer(precision_score),
+           'recall': 'recall', 'F1 Score': 'f1'}
+
+
+scores = list()
+for dataset_ratio, dataset in datasets:
     X,y = dataset
     for name, clf in classifiers:
-        grid_search = GridSearchCV(clf, param_grid=param_grid)
-        grid_search.fit(X, y)
-        print("-----{0:s}------".format(name))
-        report(grid_search.cv_results_)
-        joblib.dump(grid_search,"{0}.pkl".format(name))
+        print("compute: {0:.2f} imbalance, {1} ...".format(dataset_ratio, name), end='', flush=True)
+        this_score = cross_val_score(clf, X, y, cv=skf, n_jobs=N_JOBS, scoring=scoring)
+        scores.append((dataset_ratio, name, this_score))
+        print("done")
 
 
-import glob
-import pylab
-xx = [joblib.load(x).cv_results_['rank_test_score'] for x in glob.glob("./*.pkl")]
-pylab.pcolor(np.asarray(xx))
-yy = [ joblib.load(x).cv_results_['mean_test_score'] for x in glob.glob("./*.pkl")]
-pylab.pcolor(np.asarray(yy))
+from sklearn.externals import joblib
+joblib.dump(scores, "scores.pkl")
+
+print('done')
